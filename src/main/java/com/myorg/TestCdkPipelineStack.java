da8f6a99bfe93453dd73d346c5326fc28075bdf7
Vinícius Carneiro <vinicius.carneiro@caylent.com>
2023-09-18 12:56:26 -0300

    test creating an SQS queue

--- src/main/java/com/myorg/TestCdkPipelineStack.java
+++ src/main/java/com/myorg/TestCdkPipelineStack.java
@@ -17,8 +17,8 @@ public class TestCdkPipelineStack extends Stack {
         // The code that defines your stack goes here
 
         // example resource
-        // final Queue queue = Queue.Builder.create(this, "TestCdkPipelineQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        final Queue queue = Queue.Builder.create(this, "TestCdkPipelineQueue")
+                .visibilityTimeout(Duration.seconds(300))
+                .build();
     }
 }
